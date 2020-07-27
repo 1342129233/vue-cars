97ca2647f78240aff26e46d615cddd4aed40e605
wangxin <1342129233@qq.com>
2020-07-27 19:53:14 +0800

    elementui

--- src/main.js
+++ src/main.js
@@ -13,6 +13,15 @@ Vue.prototype.$axios = axios;
 // 高德地图
 import "./plugin/aMap";
 
+// elementui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI, {
+  size: 'medium',
+  i18n: (key, value) => i18n.t(key, value)
+})
+
 Vue.config.productionTip = false;
 
 new Vue({
